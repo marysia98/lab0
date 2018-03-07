e41c68f7ffca27142f85efb5a5e1529b08aa1309
Unknown <maria_jarek@wp.pl>
2018-03-07 19:37:41 +0100

    pierwszy

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "fifo.h"
+
+
+int main() {
+
+	fifo_string test_obj;
+	test_obj.push("PROI1");
+	test_obj.push("PROI2");
+	test_obj.push("PROI3");
+	test_obj.push("PROI4");
+
+	test_obj.take();
+
+        test_obj.push("PROI5");
+        test_obj.push("PROI6");
+
+	test_obj.get(100);
+	std::cout<<test_obj;
+	return 0;
+}
